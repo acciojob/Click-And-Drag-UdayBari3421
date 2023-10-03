d5dd0edc0191b2eb72a3ac00f7a1510e002fa26d
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-10-03 08:43:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// Your code here.
+const source = document.getElementById("parent");
+const childElement = source.querySelectorAll("div");
+
+let dragged;
+
+for (let i = 0; i < childElement.length; i++) {
+	childElement[i].draggable = "true";
+}
+
+source.addEventListener("drag", (event) => {
+  console.log("dragging");
+});
+
+const target = document.querySelectorAll(".item");
+target.addEventListener(
+  "dragover",
+  (event) => {
+    // prevent default to allow drop
+    event.preventDefault();
+  },
+  false,
+);
