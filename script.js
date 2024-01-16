14ffbd1ea7f06d0c6d7d922fa9f6c1e11a6304b2
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-01-16 11:24:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
 // Your code here.
-const items = document.querySelectorAll('.item')
-const container = document.querySelector('.items')
-items.forEach(el=>{
-	// el.draggable = "true"
-	el.addEventListener('mouseup',()=>{
-		container.scrollLeft +=50
-	})
-	el.addEventListener('mousedown',()=>{
-		container.scrollLeft -=50
-	})
-})
